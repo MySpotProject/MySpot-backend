0a4ea287248f3296d172ef50a5248b455bcae4d7
vernicode <65769471+vernicode@users.noreply.github.com>
2023-06-13 18:02:45 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.hosts << "konstabx.beget.tech"
+  config.hosts << ENV["HOST"]
 
   Rails.application.routes.default_url_options = {
     host: ENV["HOST"]
