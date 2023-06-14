26a6bca452fc0a925d5db81b80b8fa9091ffe295
vernicode <konstantinvernigorov.kot03@gmail.com>
2023-06-15 01:21:31 +0300

    Dependencies destroy in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 
-  has_one_attached :avatar
+  has_one_attached :avatar, dependent: :destroy
   has_many :accounts, dependent: :destroy
-  has_many :spot_ratings
-  has_many :user_ratings
-  has_many :comments
+  has_many :spot_ratings, dependent: :destroy
+  has_many :user_ratings, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :avatar, content_type: ['image/png', 'image/jpeg'], size: { between: 1.kilobyte..5.megabytes , message: 'is not given between size' }
 end
